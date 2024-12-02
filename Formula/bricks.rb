32c1b0c02690183376bea22ee644a6bdf43febd9
goreleaserbot <goreleaserbot@bluebricks.co>
2024-12-02 10:31:36 +0200

    [skip ci] Bot: Brew formula update for bricks version v1.13.0

--- Formula/bricks.rb
+++ Formula/bricks.rb
@@ -5,12 +5,12 @@
 class Bricks < Formula
   desc "Bricks CLI - Infrastructure Control Plane and Orchestration"
   homepage "https://bluebricks.co"
-  version "1.12.6"
+  version "1.13.0"
   license ":cannot_represent"
 
   on_macos do
-    url "https://brickscli.s3.eu-west-1.amazonaws.com/releases/1.12.6/bricks_1.12.6_darwin_all.tar.gz"
-    sha256 "7e143542d762b3d4e200ec61185775f8ff48bed7eaae75388dade2ef60f1cfc2"
+    url "https://brickscli.s3.eu-west-1.amazonaws.com/releases/1.13.0/bricks_1.13.0_darwin_all.tar.gz"
+    sha256 "f597955fa7e59d44a938446e766ace5e829ead0f367e99b4d421c5389ab59d95"
 
     def install
       bin.install "bricks"
@@ -20,8 +20,8 @@ class Bricks < Formula
   on_linux do
     on_intel do
       if Hardware::CPU.is_64_bit?
-        url "https://brickscli.s3.eu-west-1.amazonaws.com/releases/1.12.6/bricks_1.12.6_linux_amd64.tar.gz"
-        sha256 "94607abfdda18a20e4245ffd38d31cb788a9d31e770350ccf100d4abae4d6ea7"
+        url "https://brickscli.s3.eu-west-1.amazonaws.com/releases/1.13.0/bricks_1.13.0_linux_amd64.tar.gz"
+        sha256 "ac7af0329751d8021e0b690c0007c6a612ec41cedf2625b2b4660a2e0b9b6484"
 
         def install
           bin.install "bricks"
@@ -30,8 +30,8 @@ class Bricks < Formula
     end
     on_arm do
       if Hardware::CPU.is_64_bit?
-        url "https://brickscli.s3.eu-west-1.amazonaws.com/releases/1.12.6/bricks_1.12.6_linux_arm64.tar.gz"
-        sha256 "9a31977d89ad02bf43da103e02f3254d8391194f60c3a93d489f1a7d37fa23d7"
+        url "https://brickscli.s3.eu-west-1.amazonaws.com/releases/1.13.0/bricks_1.13.0_linux_arm64.tar.gz"
+        sha256 "878acc4bf12f53de7a8b3d9bf447f410d4f2bef1f6126ae93d08d42a30b71cd1"
 
         def install
           bin.install "bricks"
